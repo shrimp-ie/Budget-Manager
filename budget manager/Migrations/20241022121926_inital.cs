db614757af0ee499f5d8867a84cad7438aefc307
mack <2352203@live.barnsley.ac.uk>
2024-10-22 13:20:14 +0100

    Add project files.

---
+++ budget manager/Migrations/20241022121926_inital.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace budget_manager.Migrations
+{
+    public partial class inital : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Expenses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Income = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expenses", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Expenses");
+        }
+    }
+}
---
+++ budget manager/Models/Expenses.cs
@@ -0,0 +1,11 @@
+﻿namespace BudgetManager.Models
+{
+    public class Expenses
+    {
+
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public decimal Income { get; set; }
+
+    }
+}
